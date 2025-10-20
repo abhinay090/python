c7cbe7ddcaa842243b9f103f2de177fb8095d0ee
abhinay090 <168563318+abhinay090@users.noreply.github.com>
2025-10-20 22:52:51 +0530

    Update Fibonacci_Sequence.py

--- algorithms/math/Fibonacci_Sequence.py
+++ algorithms/math/Fibonacci_Sequence.py
@@ -1,12 +1,13 @@
 # Fibonacci Sequence
 
-if __name__ == '__main__':
-    f = [0, 1]
-    index1=0
-    index2=1
-    fibonacci_len = 10 # Lenght of Fibonacci Sequence
-    for n in range(fibonacci_len):
-        val = f[index1+n]+f[index2+n]
-        f.append(val)
-    f.pop(0)
-    print f
+def fib(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    seq=[0,1]
+    for i in range(2,n):
+        seq.append(seq[-1]+seq[-2])
+    return seq
+n=int(input())
+print(fib(n))
